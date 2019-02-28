28658ca4f69415165d530c93b3948bd92f1a3611
anagajyothi <47244096+anagajyothi@users.noreply.github.com>
2019-02-28 15:06:14 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,9 +1,11 @@
-<?php
-$con = mysqli_connect('localhost','root','mysql');
-if(!$con)
-{
-echo "not connected to server";
+$database = "jyothi";
+$username = "root";
+$password = "mysql";
+$conn = mysqli_connect($servername, $username, $password, $database);
+if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
 }
+
 
 $CustomerId = $_POST['cid'];
 $Name = $_POST['ccfname'];
@@ -11,13 +13,13 @@ $lName = $_POST['cclname'];
 $Mobile = $_POST['ccmobile'];
 $Email = $_POST['ccemail'];
 $Address = $_POST['ccaddress'];
-$sql = "INSERT INTO data (cfname, clname, cmobile, cemail, caddress) VALUES ('$cfname', '$clname', '$cmobile', '$cemail', '$caddress')";
-if($con->query($sql) === TRUE)
-{
-echo "Not Inserted";
+$sql = "INSERT INTO customer (cid, cfname, clname, cmobile, cemail, caddress) VALUES ('$cid', '$cfname', '$clname', '$cmobile', '$cemail', 
+
+'$caddress')";
+if (mysqli_query($conn, $sql)) {
+      echo "New record created successfully";
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
-else
-{
-echo "Inserted";
-}
-      ?>
+mysqli_close($conn);
+?>
